a6c5e7eb50511874591c6ef7798d1ed1dfc87767
ThetHtueZaw <thetthet311255788@gmail.com>
2023-01-15 14:46:29 +0630

    Adding Dash

--- Assets/_RPG/Modules/AbilitySystem/Scripts/DashAbility.cs
+++ Assets/_RPG/Modules/AbilitySystem/Scripts/DashAbility.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "RPG/AbilitySystem/DashAbility")]
 public class DashAbility : Ability
 {
+    public float DashForce;
+    public bool IsDashing;
+
     public override void Activate(GameObject activator)
     {
         base.Activate(activator);
+        Vector3 forceToApply = activator.transform.forward * DashForce;
+        activator.GetComponent<Rigidbody>().AddForce(forceToApply, ForceMode.Impulse);
+        activator.GetComponentInChildren<Animator>().SetTrigger("Dash");
     }
+
+    
 }
